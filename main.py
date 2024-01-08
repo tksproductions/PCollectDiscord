c5aaca536f28eade996a5938d40c08e29612a3a3
tksproductions <130306272+tksproductions@users.noreply.github.com>
2024-01-07 23:36:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def extract_photos(input_image, aspect_ratio=(5.5, 8.5), min_percentage=0.5):
 
 ## CLIENT ##
 client = commands.Bot(command_prefix=".", intents = discord.Intents.all(), activity=discord.Activity(type=discord.ActivityType.listening, name="Ditto"))
-GIVEAWAY_MESSAGE_ID = None 
+GIVEAWAY_MESSAGE_ID = 1193775218230054932 
 
 @client.event
 async def on_ready():
